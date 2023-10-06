5c9ae4f1cc4e33c9773d12aa34c83b9c59fa3b88
coutJP <118437370+coutJP@users.noreply.github.com>
2023-10-06 18:06:13 +0300

    build

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p className='message'>Hello, my name is chadi and this website is for tattoo barcode scan</p>
+      <p className='message'>
+      Hello, my name is chadi and this 
+      website is for tattoo barcode scan and jeanpaul did  it!
+      </p>
    
     <div>
       <img src='./img1.jpg' alt='testimg' className='myimg'/>
+    </div>
+    <div>
+      <img src='./img2.jpg' alt='testimg' className='myimg'/>
     </div>
     <p className='copyR'>Copyright © JeanPaul Mansour, we did it in 1 min 	&#128513; </p>
     </div>
