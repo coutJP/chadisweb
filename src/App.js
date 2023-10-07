04e9a5e1deb2c06c2e75e186dc9d846a233c886e
coutJP <118437370+coutJP@users.noreply.github.com>
2023-10-07 13:15:38 +0300

    img flip

--- src/App.js
+++ src/App.js
@@ -9,13 +9,32 @@ function App() {
       Hello, my name is chadi and this 
       website is for tattoo barcode scan and jeanpaul did  it!
       </p>
-   
-    <div>
-      <img src={img1} alt='testimg' className='myimg'/>
+      <div className='container'>
+      <div class="flip-box">
+        <div class="flip-box-inner">
+        <div class="flip-box-front">
+       <img src={img1} alt="Paris" className='myimg'/>
     </div>
-    <div>
-      <img src={img2} alt='testimg' className='myimg'/>
+    <div class="flip-box-back">
+      <h2>We Plan For It</h2>
+      <p>TRALALA</p>
     </div>
+  </div>
+  </div>
+      <div class="flip-box">
+        <div class="flip-box-inner">
+        <div class="flip-box-front">
+       <img src={img2} alt="Paris" className='myimg'/>
+    </div>
+    <div class="flip-box-back">
+      <h2>I did it</h2>
+      <p>WOHOO</p>
+    </div>
+  </div>
+  </div>
+  </div>
+
+
     <p className='copyR'>Copyright © JeanPaul Mansour, we did it in 1 min 	&#128513; </p>
     </div>
   );
